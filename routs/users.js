5137a838ed08d222550fe28e24569bbbe27f6bee
taniaijaz <taniaijaz107@gmail.com>
2020-03-06 22:55:58 +0500

    my first commit

--- routs/users.js
+++ routs/users.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const router=express.Router();
 //Login Page
-router.get('/login',(req,res)=>res.send('Welcome Dear Its your First Attempt to login'));
+router.get('/login',(req,res)=>res.render('login'));
 //Register Page
 router.get('/register',(req,res)=>res.render('register'));
 //Register Handel
