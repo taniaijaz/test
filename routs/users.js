1bd1113e1e57ba64da4f87f99b30c3d72329500d
taniaijaz <taniaijaz107@gmail.com>
2020-03-07 23:09:13 +0500

    register validation

--- routs/users.js
+++ routs/users.js
@@ -6,7 +6,35 @@ router.get('/login',(req,res)=>res.render('login'));
 router.get('/register',(req,res)=>res.render('register'));
 //Register Handel
 router.post('/register',(req,res)=>{
-    Console.log(req.body);
-    res.send('hello');
+    const {name,email,password,password2 }=req.body;
+    let errors=[];
+    //check Required Fields
+    if(!name|| !email || !password || !password2){
+        errors.push({msg:'please fil in all  fields'});
+
+    }
+    //Check Password matche
+    if(password !== password2){
+        errors.push({msg:'Password Do not match'});
+    }
+    //check Length
+    if(password.length<5){
+        errors.push({msg:'Password must be at least 5 characters'});
+    }
+if(errors.length>0){
+
+res.render('register',{
+    errors,
+    name,
+    email,
+    password,
+    password2
+})
+
+}
+else{
+    res.send('pass');
+}
+    
 });
 module.exports=router;
